e3068c9f78e65834231a4f0571c015c519b3bee7
Sarthak Joshi <joshi.sarthak@gmail.com>
2021-09-13 20:07:19 -0400

    Styling & Data Changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 // src/components/Contact.js
 
-import { React, useState } from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
     const [name, setName] = useState("");
@@ -47,8 +47,8 @@ export default function Contact() {
                                 ADDRESS
                             </h2>
                             <p className="mt-1">
-                                97 Warren St. <br />
-                                New York, NY 10007
+                                123 Hello World <br />
+                                Charleston 29414
                             </p>
                         </div>
                         <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -56,7 +56,7 @@ export default function Contact() {
                                 EMAIL
                             </h2>
                             <a className="text-indigo-400 leading-relaxed">
-                                reedbarger@email.com
+                                joshi.sarthak@gmail.com
                             </a>
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                                 PHONE
@@ -66,7 +66,7 @@ export default function Contact() {
                     </div>
                 </div>
                 <form
-                    netlify
+                    netlify="true"
                     name="contact"
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -74,7 +74,7 @@ export default function Contact() {
                         Hire Me
                     </h2>
                     <p className="leading-relaxed mb-5">
-                        This is selection form to contact me 
+                        This is selection form to contact me
                     </p>
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
--- src/data.js
+++ src/data.js
@@ -35,6 +35,7 @@ export const projects = [
 
 export const testimonials = [
   {
+    uniqueId: 1,
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
     image: "https://randomuser.me/api/portraits/men/1.jpg",
@@ -42,6 +43,7 @@ export const testimonials = [
     company: "Zoozle",
   },
   {
+    uniqueId: 2,
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
     image: "https://randomuser.me/api/portraits/men/94.jpg",
